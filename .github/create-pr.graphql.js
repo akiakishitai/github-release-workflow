42f343fff8e50018e2810ea0043fb6b3e310c438
Akiaki Susuki <35886966+akiakishitai@users.noreply.github.com>
2020-12-09 15:18:55 +0900

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! feat: add workflow of creating pr

--- .github/create-pr.graphql.js
+++ .github/create-pr.graphql.js
@@ -1,19 +1,10 @@
-module.exports = async ({ github, context, options: { repoId, base, head, title, templateFile } }) => {
-  const fs = require('fs').promises
-  const { resolve } = require('path')
-
-  const query = await fs.readFile(resolve(__dirname, 'create-pr.gql'), { encoding: "utf-8" })
-  const templateFileName = 'pull_request_template.md'
-  const template = await fs.readFile(templateFile, { encoding: "utf-8" })
-    .catch((err) => fs.readFile(resolve(__dirname, templateFileName), { encoding: "utf-8" }))
-    .catch((err) => fs.readFile(resolve(__dirname, templateFileName.toUpperCase()), { encoding: "utf-8" }))
-
+module.exports = async ({ github, context, options: { repoId, base, head, title, body, query } }) => {
   const variables = {
     repoId: repoId || context.payload.repository.node_id,
     base: base,
     head: head,
     title: title,
-    body: template
+    body: body
   }
 
   return await github.graphql(query, variables)
